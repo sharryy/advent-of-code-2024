9f54cd154bb69e1a901cee847b694fd7f6061746
Sharryy <ibneadam388@gmail.com>
2025-10-02 00:09:39 +0200

    feat: Optimise day-1 part-2 with lookup map

--- day1/b.c
+++ day1/b.c
@@ -5,9 +5,24 @@
 
 #define MAX_LOCATIONS 2000
 
+typedef struct
+{
+    int value;
+    int count;
+} frequency_t;
+
 int comparator(const void* a, const void* b)
 {
     return *(int*)a - *(int*)b;
+}
+
+int freq_comparator(const void* a, const void* b)
+{
+    const int key = *(int*)a;
+
+    const frequency_t* item = b;
+
+    return key - item->value;
 }
 
 int main()
@@ -37,25 +52,41 @@ int main()
 
     qsort(right_list, location_count, sizeof(int), comparator);
 
+    frequency_t frequency[MAX_LOCATIONS];
+
+    int unique_count = 0;
+
+    if (location_count > 0)
+    {
+        frequency[0].value = right_list[0];
+        frequency[0].count = 1;
+        unique_count = 1;
+
+        for (int i = 1; i < location_count; i++)
+        {
+            if (right_list[i] == right_list[i - 1])
+            {
+                frequency[unique_count - 1].count++;
+            }
+            else
+            {
+                frequency[unique_count].value = right_list[i];
+                frequency[unique_count].count = 1;
+                unique_count++;
+            }
+        }
+    }
+
     for (int i = 0; i < location_count; i++)
     {
         int number_left = left_list[i];
 
-        int count_right = 0;
+        const frequency_t* found = bsearch(&number_left, frequency, unique_count, sizeof(frequency_t), freq_comparator);
 
-        for (int j = 0; j < location_count; j++)
+        if (found != NULL)
         {
-            if (number_left == right_list[j])
-            {
-                count_right++;
-            }
-            else if (number_left < right_list[j])
-            {
-                break;
-            }
+            similarity += (long long)number_left * found->count;
         }
-
-        similarity += (long long)number_left * count_right;
     }
 
     printf("%lld\n", similarity);
