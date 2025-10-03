4ec8d50da26a96442e2695cda1142eb7fcf89c38
Sharryy <ibneadam388@gmail.com>
2025-10-04 01:25:09 +0200

    feat: Add solution for day-3 (part-1)

---
+++ day3/a.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 4096
+
+int main()
+{
+    char buffer[MAX_LINE_LENGTH];
+
+    FILE* file = fopen("input.txt", "r");
+
+    if (file == NULL)
+    {
+        perror("Error opening file");
+        return 1;
+    }
+
+    long long int sum = 0;
+
+    while (fgets(buffer, sizeof(buffer), file) != NULL)
+    {
+        const size_t length = strlen(buffer);
+
+        for (size_t i = 0; i < length; i++)
+        {
+            if (strncmp(buffer + i, "mul(", 4) == 0)
+            {
+                int number1, number2;
+                int chars_consumed = 0; // To store how many characters sscanf reads
+
+                char next_char;
+
+                const int items_scanned = sscanf(buffer + i + 4, "%d,%d%c%n", &number1, &number2, &next_char, &chars_consumed);
+
+                if (items_scanned == 3 && next_char == ')')
+                {
+                    sum += (long long)number1 * number2;
+                    i += 4 + chars_consumed - 1; // The -1 is because the for loop will add 1 back on the next iteration.
+                }
+            }
+        }
+    }
+
+    fclose(file);
+    printf("%lld\n", sum);
+    return 0;
+}
